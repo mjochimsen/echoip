322078fca585dbb71abd858c25f8dacb9bdf933e
Michael Jochimsen <mlj@troglodytics.org>
2019-01-25 11:22:40 -0800

    Add tests to `Config` parsing.
    Add a couple of rudimentary tests to our `Config` parsing. This means
    that we need to be able to pass in custom lists of arguments, so we add
    an `args` parameter to `Config::parse_args()`.

--- src/bin/echoip.rs
+++ src/bin/echoip.rs
@@ -1,9 +1,11 @@
 extern crate clap;
 
 use std::net::Ipv4Addr;
+use std::env::args_os;
+use std::ffi::OsString;
 
 fn main() {
-    let config = Config::parse_args();
+    let config = Config::parse_args(args_os());
 
     println!("Hello, echoip! address = {}, port = {}",
              config.address, config.port);
@@ -19,7 +21,9 @@ impl Config {
 
     const DEFAULT_PORT: u16 = 5300;
 
-    fn parse_args() -> Config {
+    fn parse_args<I, T>(args: I) -> Config
+        where I: IntoIterator<Item = T>,
+              T: Into<OsString> + Clone {
         use clap::{App, Arg};
 
         let matches = App::new("echoip")
@@ -36,7 +40,7 @@ impl Config {
                  .help("The IP address of the echo server")
                  .required(true)
                  .validator(Config::valid_address))
-            .get_matches();
+            .get_matches_from(args);
 
         let port = match matches.value_of("port") {
             Some(val) => val.parse::<u16>().unwrap(),
@@ -60,5 +64,38 @@ impl Config {
             Ok(_) => Ok(()),
             Err(_) => Err("Not a valid IP address".to_string()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn addr() -> Ipv4Addr {
+        "1.2.3.4".parse::<Ipv4Addr>().unwrap()
+    }
+
+    #[test]
+    fn parse_address() {
+        let args = vec!["echoip", "1.2.3.4"];
+        let config = Config::parse_args(args);
+        assert_eq!(config.port, Config::DEFAULT_PORT);
+        assert_eq!(config.address, addr());
+    }
+
+    #[test]
+    fn parse_port() {
+        let args = vec!["echoip", "--port", "12345", "1.2.3.4"];
+        let config = Config::parse_args(args);
+        assert_eq!(config.port, 12345);
+        assert_eq!(config.address, addr());
+    }
+
+    #[test]
+    fn parse_help() {
+    }
+
+    #[test]
+    fn parse_version() {
     }
 }
