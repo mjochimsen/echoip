7731eb5e096c4bc28a0918d303d0a106e547ceea
Michael Jochimsen <mlj@troglodytics.org>
2019-01-30 17:53:53 -0800

    Tie the `client()` function in to the `echoip` binary.
    Finish the `echoid` binary by calling the `client()` function in the
    `echoip::client` module.

--- src/bin/echoip.rs
+++ src/bin/echoip.rs
@@ -2,10 +2,9 @@ extern crate echoip;
 
 use std::env::args_os;
 use echoip::config::ClientConfig;
+use echoip::client::client;
 
 fn main() {
     let config = ClientConfig::parse_args(args_os());
-
-    println!("Hello, echoip! address = {}, port = {}",
-             config.address, config.port);
+    client(config.address, config.port);
 }
